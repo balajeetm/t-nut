702c9946e992cec0b55ae315fc7cb8a1e249a1ed
balajeetm <balajmoh@cisco.com>
2017-03-12 23:36:00 +0530

    [Bala] Reverse all code

--- src/main/java/com/balajeetm/tnut/controller/ReverseSinglyLinkedList.java
+++ src/main/java/com/balajeetm/tnut/controller/ReverseSinglyLinkedList.java
@@ -24,6 +24,9 @@ import com.balajeetm.tnut.lever.SinglyLinkedListLever;
 import com.balajeetm.tnut.model.SNode;
 import com.google.gson.JsonArray;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 /**
  * The Class ReverseSinglyLinkedList.
  *
@@ -31,6 +34,7 @@ import com.google.gson.JsonArray;
  */
 @RestController
 @RequestMapping("/tnut/reverse-in-pockets")
+@Api(tags = { "Singly Linked List" }, description = "Reverse list's every n nodes")
 public class ReverseSinglyLinkedList {
 
 	/** The s lever. */
@@ -45,16 +49,17 @@ public class ReverseSinglyLinkedList {
 	 * @return the json array
 	 */
 	@PostMapping("/brute")
+	@ApiOperation(value = "Brute Force Approach", notes = "Move items one by one, revisiting every item")
 	public JsonArray brute(@RequestBody JsonArray array, @RequestParam(defaultValue = "1") Integer n) {
 		SNode root = sLever.getSinglyLinkedList(array);
 
-		/*variable used to traverse every node*/
+		/* variable used to traverse every node */
 		SNode node = root;
 
-		/*variable used to keep track of the node position to identify pockets*/
+		/* variable used to keep track of the node position to identify pockets */
 		Integer nodeCount = 0;
 
-		/* variables used to performing swapping within a pocket of nodes*/
+		/* variables used to performing swapping within a pocket of nodes */
 		Integer pocket = null;
 		Integer count = 0;
 
@@ -70,6 +75,9 @@ public class ReverseSinglyLinkedList {
 					count++;
 				}
 				swap(node, newPos);
+
+				/* -2 because, 2 nodes are out of the equation
+				 * one in front, and one in the end */
 				pocket = pocket - 2;
 			}
 			node = node.getNext();
@@ -87,17 +95,18 @@ public class ReverseSinglyLinkedList {
 	 * @return the json array
 	 */
 	@PostMapping("/brute-stack")
+	@ApiOperation(value = "Brute Force using Stack Approach", notes = "Stack all items in a pocket sequentially, and pop them to respective positions on second visit")
 	public JsonArray bruteStack(@RequestBody JsonArray array, @RequestParam(defaultValue = "1") Integer n) {
 		SNode root = sLever.getSinglyLinkedList(array);
 
-		/*variable used to traverse every node*/
+		/* variable used to identify the current pocket of nodes we are dealing with */
 		SNode currentRoot = root;
 		SNode node = root;
 
-		/*variable used to keep track of the node position to identify pockets*/
+		/* variable used to keep track of the node position to identify pockets */
 		Integer nodeCount = 0;
 
-		/* variables used to performing swapping within a pocket of nodes*/
+		/* stack used to place nodes in appropriate positions */
 		Stack<Integer> stack = new Stack<>();
 
 		while (null != node) {
@@ -113,6 +122,80 @@ public class ReverseSinglyLinkedList {
 		relocate(stack, currentRoot);
 
 		return sLever.getJsonArray(root);
+	}
+
+	/**
+	 * Reverse full list.
+	 *
+	 * @param array the array
+	 * @return the json array
+	 */
+	@PostMapping("/fullreverse")
+	@ApiOperation(value = "Recursive Approach", notes = "Reverse the list with a single traversal of nodes")
+	public JsonArray reverseFullList(@RequestBody JsonArray array) {
+		SNode root = sLever.getSinglyLinkedList(array);
+		return sLever.getJsonArray(fullReverse(root));
+	}
+
+	/**
+	 * Reverse in pockets.
+	 *
+	 * @param array the array
+	 * @param n the n
+	 * @return the json array
+	 */
+	@PostMapping("/pocketreverse")
+	@ApiOperation(value = "Recursive Approach in pockets", notes = "Reverse the list with a single traversal of nodes")
+	public JsonArray reverseInPockets(@RequestBody JsonArray array, @RequestParam(defaultValue = "1") Integer n) {
+		SNode root = sLever.getSinglyLinkedList(array);
+		return sLever.getJsonArray(pocketReverse(root, n));
+	}
+
+	/**
+	 * Pocket reverse.
+	 *
+	 * @param root the root
+	 * @param pocketSize the pocket size
+	 * @return the s node
+	 */
+	private SNode pocketReverse(SNode root, Integer pocketSize) {
+		Integer count = 0;
+		SNode prev = null;
+		SNode curr = null;
+		SNode next = null;
+
+		curr = root;
+		while (count < pocketSize && null != curr) {
+			next = curr.getNext();
+			curr.setNext(prev);
+			prev = curr;
+			curr = next;
+			count++;
+		}
+
+		root.setNext(null == curr ? curr : pocketReverse(curr, pocketSize));
+		return prev;
+	}
+
+	/**
+	 * Full reverse.
+	 *
+	 * @param root the root
+	 * @return the s node
+	 */
+	private SNode fullReverse(SNode root) {
+		SNode prev = null;
+		SNode curr = null;
+		SNode next = null;
+
+		curr = root;
+		while (null != curr) {
+			next = curr.getNext();
+			curr.setNext(prev);
+			prev = curr;
+			curr = next;
+		}
+		return prev;
 	}
 
 	/**
